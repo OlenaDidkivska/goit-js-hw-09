f490625e7efd2a45dcc89c90186d4656b8e2b633
OlenaDidkivska <99613214+OlenaDidkivska@users.noreply.github.com>
2022-08-24 20:16:32 +0000

    Deploying to gh-pages from @ OlenaDidkivska/goit-js-hw-09@3a7c100715bcf0ab5f4cf8f13f65150248ab4f53 🚀

---
+++ 01-color-switcher.60832d72.js
@@ -0,0 +1,2 @@
+!function(){var t={bodyEl:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};t.btnStart.addEventListener("click",(function(){timerId=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));t.bodyEl.style.background=e}),1e3),t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(function(){clearInterval(timerId),t.btnStart.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.60832d72.js.map
