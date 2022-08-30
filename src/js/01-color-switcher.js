8420860003aebf506413cacf68deb299df23f11d
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-08-30 21:23:43 +0300

    add

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    refs.bodyEl.style.background = color;
+  }, 1000);
+  refs.btnStart.disabled = true;
+}
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+}
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
