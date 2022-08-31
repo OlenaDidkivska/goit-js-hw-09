964635cf48d66cc251246ac7ac68159ab1e1854c
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-09-01 00:04:27 +0300

    add promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,59 @@
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      console.log(position, delay);
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formEl = event.currentTarget.elements;
+  const delayEl = Number(formEl.delay.value);
+  const stepEl = Number(formEl.step.value);
+  const amount = Number(formEl.amount.value);
+
+  let positions = [];
+  let delay;
+
+  for (let i = 0; i < amount; i += 1) {
+    const element = i + 1;
+    positions.push(element);
+  }
+
+  for (let i = delayEl; i < positions.length + 1; i += stepEl) {
+    delay += i;
+  }
+
+  const promises = positions
+    .map(position => createPromise(position, 1000))
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+  //   (position, delay)
+  //     .then(({ position, delay }) => {
+  //       console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  //     })
+  //     .catch(({ position, delay }) => {
+  //       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  //     })
+  // );
+  Promise.race(promises);
+
+  console.log(promises);
+}
+
+form.addEventListener('submit', onFormSubmit);
